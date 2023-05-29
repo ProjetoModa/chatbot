ce37df6f96e801f8da0bb41629b02e3f2a2f44f8
arbezerra <arbezerra@gmail.com>
2023-05-29 16:30:58 -0300

    Add intent model

--- chatbot/intent.py
+++ chatbot/intent.py
@@ -3,14 +3,18 @@ import os
 
 class IntentClassifier:
     def __init__(self):
-        #TODO import model
-        # self.preprocessor = self._load(os.path.join(os.path.dirname(__file__), "preprocessor.pkl"))
-        # self.model = self._load(os.path.join(os.path.dirname(__file__), "model.pkl"))
-        pass
+        self.label_encoder = self._load(os.path.join(os.path.dirname(__file__), "label.pkl"))
+        self.tfidf_encoder = self._load(os.path.join(os.path.dirname(__file__), "tfidf.pkl"))
+        self.model = self._load(os.path.join(os.path.dirname(__file__), "model.pkl"))
+        self.label_dict = dict(zip(self.label_encoder.classes_, self.label_encoder.transform(self.label_encoder.classes_)))
+    
+    def getLabel(self, label):
+        return self.label_dict(label)
+        
     def _load(self, filename):
         file = open(filename, "rb")
         data = pkl.load(file)
         file.close()
         return data
     def predict(self, text):
-        return self.model.predict_proba(self.preprocessor.transform([text])).tolist()[0]
+        return self.model.predict_proba(self.tfidf_encoder.transform([text])).tolist()[0]
